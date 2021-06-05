8e9a605b9308415af3cfe871627c977ed9946e63
Andrew <81682101+Myakis@users.noreply.github.com>
2021-06-05 14:59:51 +1000

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,120 @@
+
+
+//advantages
+
+$(window).scroll(function () {
+  $('.advantages__title').each(function () {
+    var imagePos = $(this).offset().top;
+
+    var topOfWindow = $(window).scrollTop();
+    if (imagePos < topOfWindow + 1000) {
+      $(this).addClass("fadeInLeft");
+    }
+  });
+});
+
+$(window).scroll(function () {
+  $('.item-one').each(function () {
+    var imagePos = $(this).offset().top;
+
+    var topOfWindow = $(window).scrollTop();
+    if (imagePos < topOfWindow + 1000) {
+      $(this).addClass("bounceInLeft");
+    }
+  });
+});
+
+$(window).scroll(function () {
+  $('.item-two').each(function () {
+    var imagePos = $(this).offset().top;
+
+    var topOfWindow = $(window).scrollTop();
+    if (imagePos < topOfWindow + 1000) {
+      $(this).addClass("bounceInDown");
+    }
+  });
+});
+
+$(window).scroll(function () {
+  $('.item-three').each(function () {
+    var imagePos = $(this).offset().top;
+
+    var topOfWindow = $(window).scrollTop();
+    if (imagePos < topOfWindow + 1000) {
+      $(this).addClass("bounceInRight");
+    }
+  });
+});
+
+
+//dream
+
+$(window).scroll(function () {
+  $('.dream__title').each(function () {
+    var imagePos = $(this).offset().top;
+
+    var topOfWindow = $(window).scrollTop();
+    if (imagePos < topOfWindow + 1000) {
+      $(this).addClass("rollIn");
+    }
+  });
+});
+
+
+$(window).scroll(function () {
+  $('.input-1').each(function () {
+    var imagePos = $(this).offset().top;
+
+    var topOfWindow = $(window).scrollTop();
+    if (imagePos < topOfWindow + 1000) {
+      $(this).addClass("fadeInLeftBig");
+    }
+  });
+});
+$(window).scroll(function () {
+  $('.input-2').each(function () {
+    var imagePos = $(this).offset().top;
+
+    var topOfWindow = $(window).scrollTop();
+    if (imagePos < topOfWindow + 1000) {
+      $(this).addClass("fadeInRightBig");
+    }
+  });
+});
+$(window).scroll(function () {
+  $('.input-3').each(function () {
+    var imagePos = $(this).offset().top;
+
+    var topOfWindow = $(window).scrollTop();
+    if (imagePos < topOfWindow + 1000) {
+      $(this).addClass("fadeInLeftBig");
+    }
+  });
+});
+$(window).scroll(function () {
+  $('.form__textarea').each(function () {
+    var imagePos = $(this).offset().top;
+
+    var topOfWindow = $(window).scrollTop();
+    if (imagePos < topOfWindow + 1000) {
+      $(this).addClass("fadeInRightBig");
+    }
+  });
+});
+$(window).scroll(function () {
+  $('.dream__button').each(function () {
+    var imagePos = $(this).offset().top;
+
+    var topOfWindow = $(window).scrollTop();
+    if (imagePos < topOfWindow + 1000) {
+      $(this).addClass("heartBeat");
+    }
+  });
+});
+
+
+//footer
+
+
+
+
